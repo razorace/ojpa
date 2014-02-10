520f9b9df55581f09a829a650acf223b0b265805
briarbear7@gmail.com <briarbear7@gmail.com@cb2ecf28-52c4-d167-67d8-c2e6a2916e25>
2014-02-10 15:06:58 +0000

    It now compiles (and runs) as C code whoo.

--- trunk/Enhanced/source/ui/ui_saber.cpp
+++ trunk/Enhanced/source/ui/ui_saber.cpp
@@ -12,25 +12,4 @@ USER INTERFACE SABER LOADING & DISPLAY CODE
 #include "ui_local.h"
 #include "ui_shared.h"
 
-void UI_CacheSaberGlowGraphics( void )
-{//FIXME: these get fucked by vid_restarts
-	redSaberGlowShader			= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/red_glow" );
-	redSaberCoreShader			= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/red_line" );
-	orangeSaberGlowShader		= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/orange_glow" );
-	orangeSaberCoreShader		= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/orange_line" );
-	yellowSaberGlowShader		= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/yellow_glow" );
-	yellowSaberCoreShader		= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/yellow_line" );
-	greenSaberGlowShader		= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/green_glow" );
-	greenSaberCoreShader		= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/green_line" );
-	blueSaberGlowShader			= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/blue_glow" );
-	blueSaberCoreShader			= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/blue_line" );
-	purpleSaberGlowShader		= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/purple_glow" );
-	purpleSaberCoreShader		= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/purple_line" );
-	//[RGBSabers]
-	rgbSaberGlowShader		= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/rgb_glow" );
-	rgbSaberCoreShader		= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/rgb_line" );
-	rgbSaberCore2Shader		= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/rgb_core" );
-	blackSaberGlowShader		= trap_R_RegisterShaderNoMip( "gfx/effects/sabers/black_glow" );
-	//[/RGBSabers]
 
-}
